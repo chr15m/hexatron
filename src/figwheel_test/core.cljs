02624e64cbb769204c7d10a5181696bd129eb7cb
Chris McCormick <chris@mccormick.cx>
2015-01-16 23:04:26 +0800

    Got the spinning cube working with async channels working as the requestAnimationFrame callback.

--- src/figwheel_test/core.cljs
+++ src/figwheel_test/core.cljs
@@ -1,11 +1,18 @@
 (ns figwheel-test.core
   (:require
-    [figwheel.client :as fw]))
+    [figwheel.client :as fw]
+    [cljs.core.async :refer [put! chan <! >! alts! timeout close!]]
+  )(:require-macros [cljs.core.async.macros :refer [go]]))
 
 (enable-console-print!)
 
 ;; define your app data so that it doesn't get over-written on reload
 ;; (defonce app-data (atom {}))
+
+(defn next-frame-channel [] (let [c (chan)]
+                          (js/requestAnimationFrame (fn [] (put! c 1)))
+                          c
+                          ))
 
 (defn ^:export example []
   (let [scene (js/THREE.Scene.)
@@ -13,22 +20,27 @@
         height (.-innerHeight js/window)
         camera (js/THREE.PerspectiveCamera. 75 (/ width height) 0.1 1000 )
         renderer (js/THREE.WebGLRenderer.)
-        geometry (js/THREE.BoxGeometry. 1 1 1)
-        material (js/THREE.MeshBasicMaterial. (clj->js {:color 0x00ff00}))
-        cube (js/THREE.Mesh. geometry material)
-        render (fn cb []
-                   (js/requestAnimationFrame cb) 
-                   (set! (.-x (.-rotation cube))  (+ 0.1 (.-x (.-rotation cube))) )
-                   (set! (.-y (.-rotation cube))  (+ 0.1 (.-y (.-rotation cube))) )
-                   (.render renderer scene camera)
-                 ) 
         ]
     (.log js/console "Hello.")
     (.setSize renderer width height)
     (.appendChild js/document.body (.-domElement renderer) )
-    (.add scene cube)
     (set! (.-z (.-position camera))  5)
-    (render)
+    ; (render)
+    (go 
+      (let [
+        geometry (js/THREE.BoxGeometry. 1 1 1)
+        material (js/THREE.MeshBasicMaterial. (clj->js {:color 0x00ff00}))
+        cube (js/THREE.Mesh. geometry material)
+      ]
+      (.add scene cube)
+      (loop [x 0]
+        (<! (next-frame-channel))
+        (set! (.-x (.-rotation cube)) (.sin js/Math (/ x 100)))
+        (set! (.-y (.-rotation cube)) (.sin js/Math (/ x 80)))
+        (.render renderer scene camera)
+        (recur (inc x))
+        )
+      ))
     )
   )
 
