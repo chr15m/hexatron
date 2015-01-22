a50b45a5922b8badb9e3241b20d0cb5192ed6b86
Chris McCormick <chris@mccormick.cx>
2015-01-22 20:41:28 +0800

    Test lines for nesia module commented out.

--- src/cljs_3js/core.cljs
+++ src/cljs_3js/core.cljs
@@ -5,11 +5,25 @@
     [cljs-3js.tile :as tile]
     [cljs-3js.raf :as raf]
     [cljs-3js.renderer :as renderer]
+    [nesia :as nesia]
     [cljs.core.async :refer [put! chan <! >! alts! timeout close!]]
   )(:require-macros [cljs.core.async.macros :refer [go]]))
 
 
 (enable-console-print!)
+
+; (nesia/remember! "my-data" {:one ["small" "array" "of" "joy"] :other "Yes quite good" :extra 42})
+
+; (println "extra:" (:extra (nesia/recall! "my-data")))
+; (println "other:" (:other (nesia/recall! "my-data")))
+; (println "one:" (:one (nesia/recall! "my-data")))
+; (println "nah:" (:nah (nesia/recall! "my-data")))
+
+; (println "non-existent key:" (nesia/recall! "poopstick"))
+
+; (nesia/remember! {:cats "great"} {:ok 1 :cool 3})
+
+; (println "cat's, great:" (nesia/recall! {:cats "great"}))
 
 ;; define your app data so that it doesn't get over-written on reload
 ;; (defonce app-data (atom {}))
@@ -31,19 +45,19 @@
 
 ; whenever setup happens run the 
 (defn setup []
-  (println "(setup)")
+  ; (println "(setup)")
   (ecs/run-system :setup (:entities game-state))
   )
 
 (defn teardown []
-  (println "(teardown)")
+  ; (println "(teardown)")
   (ecs/run-system :teardown (:entities game-state))
   )
 
 (fw/start {
   :on-jsload (fn []
     ;; (stop-and-start-my app)
-    (println "js-reload")
+    ; (println "js-reload")
     (teardown)
     (setup)
     )
