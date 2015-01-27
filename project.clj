ceb6a3bf059dd0aaf4e4461c9b2947327ceeeb25
Chris McCormick <chris@mccormick.cx>
2015-01-27 20:51:06 +0800

    Upgraded figwheel to latest version supporting an interactive repl.

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2411"]
-                 [figwheel "0.1.7-SNAPSHOT"]
+                 [org.clojure/clojurescript "0.0-2665"]
+                 [figwheel "0.2.2-SNAPSHOT"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [sablono "0.2.22"]
                  [om "0.8.0-beta2"]]
 
-  :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-figwheel "0.1.7-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.0.4"]
+            [lein-figwheel "0.2.2-SNAPSHOT"]]
 
   :source-paths ["src"]
   
@@ -22,17 +22,37 @@
               :compiler {:output-to "resources/public/js/compiled/hexatron.js"
                          :output-dir "resources/public/js/compiled/out"
                          :optimizations :none
-                         :source-map true}}
+                         :source-map true
+                         :source-map-timestamp true
+                         :cache-analysis true }}
              {:id "min"
               :source-paths ["src"]
-              :compiler {:output-to "www/hexatron.min.js"
+              :compiler {:output-to "resources/public/hexatron.min.js"
                          :optimizations :advanced
-                         :pretty-print false
-                         :preamble [""]
-                         :externs [""]}}]}
+                         :pretty-print false}}]}
+
   :figwheel {
              :http-server-root "public" ;; default and assumes "resources" 
              :server-port 3449 ;; default
              :css-dirs ["resources/public/css"] ;; watch and update CSS
-             ;; :ring-handler figwheel-test.server/handler
+
+             ;; Server Ring Handler (optional)
+             ;; if you want to embed a ring handler into the figwheel http-kit
+             ;; server
+             ;; :ring-handler hello_world.server/handler
+
+             ;; To be able to open files in your editor from the heads up display
+             ;; you will need to put a script on your path.
+             ;; that script will have to take a file path and a line number
+             ;; ie. in  ~/bin/myfile-opener
+             ;; #! /bin/sh
+             ;; emacsclient -n +$2 $1
+             ;;
+             ;; :open-file-command "myfile-opener"
+
+             ;; if you want to disable the REPL
+             ;; :repl false
+
+             ;; to configure a different figwheel logfile path
+             ;; :server-logfile "tmp/logs/figwheel-logfile.log" 
              })
