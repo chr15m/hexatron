04e16fde1cef740be7982e6e2138f543e40e0072
Chris McCormick <chris@mccormick.cx>
2015-02-02 21:12:46 +0800

    Forgot this file in a previous commit.

---
+++ src/hexatron/store.cljs
@@ -0,0 +1,13 @@
+(ns store
+  (:require [cljs.reader :as edn]))
+
+(defn remember! [item-name values]
+  (.setItem js/window.localStorage (pr-str item-name) (pr-str values)))
+
+(defn recall [item-name]
+  (if-let [result (.getItem js/window.localStorage (pr-str item-name))]
+    (edn/read-string result)))
+
+(defn forget! [item-name]
+  (.removeItem js/window.localStorage (pr-str item-name)))
+
