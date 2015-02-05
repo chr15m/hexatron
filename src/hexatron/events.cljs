aae40d1e5e6e072cea2020244631696d5223dd15
Chris McCormick <chris@mccormick.cx>
2015-02-05 08:43:45 +0800

    Refactored mousemove event listening code to use a channel, and a loop/recur block to only update 3d picked objects that have changed.

--- src/hexatron/events.cljs
+++ src/hexatron/events.cljs
@@ -11,6 +11,14 @@
     (js/requestAnimationFrame (fn [] (put! c (now))))
     c))
 
+(defn mouse-move []
+  (let [c (chan)]
+  (.addEventListener js/document "mousemove" (fn [ev]
+        (.preventDefault ev)
+        (put! c [(.-clientX ev) (.-clientY ev)])
+      ) false)
+    c))
+
 ; http://threejs.org/examples/canvas_interactive_cubes.html
 (defn set-picker-listener [engine interactive-entities listener]
   (let [
@@ -19,26 +27,20 @@
     renderer (:renderer engine)
     camera (:camera engine)
     entities (clj->js (map (fn [t] (set! (.-cljo (:mesh t)) t) (:mesh t)) interactive-entities))
+    mouse-move-chan (mouse-move)
         ]
-    ; (println (.-cljo  (nth entities 0)))
-    ; TODO: use a channel loop to listen for mouse event & only unpick old objects
-    (.addEventListener js/document "mousemove" (fn [ev]
-        (.preventDefault ev)
-        (set! (.-x mouse) (- (* (/ (.-clientX ev) (.-width (.-domElement renderer))) 2) 1))
-        (set! (.-y mouse) (+ (* (* (/ (.-clientY ev) (.-height (.-domElement renderer))) 2) -1) 1))
-        ; (println "mouse" (.-x mouse) (.-y mouse))
-        (.setFromCamera raycaster mouse camera)
-        (let [intersects (.intersectObjects raycaster entities)]
-          ; (println "intersects" intersects)
-          (when (count intersects) 
-            ;(dorun (map (fn [e] (put! (:channel e) "unpicked")) interactive-entities))
-            (dorun (map (fn [e] ((:unpick e) e)) interactive-entities))
-            )
-          ; (dorun (map (fn [o] (put! (:channel (.-cljo (.-object o))) "picked")) intersects))
-          (dorun (map (fn [o]
-            (let [e (.-cljo (.-object o))] ( (:pick e) e))
-            ) intersects))
-          )
-      ) false)
+    (go (loop [old-intersected-objects []]
+        (let [[x y] (<! mouse-move-chan)]
+          (set! (.-x mouse) (- (* (/ x (.-width (.-domElement renderer))) 2) 1))
+          (set! (.-y mouse) (+ (* (* (/ y (.-height (.-domElement renderer))) 2) -1) 1))
+          (.setFromCamera raycaster mouse camera)
+          (let [intersected-meshes (.intersectObjects raycaster entities)
+                intersected-objects (map (fn [m] (.-cljo (.-object m))) intersected-meshes)
+                ]
+            (dorun (map (fn [o] ((:unpick o) o)) old-intersected-objects))
+            (dorun (map (fn [o] ((:pick o) o)) intersected-objects))
+            (recur intersected-objects)
+          ))
+        ))
     )
   )
