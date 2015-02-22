33bb39307ca3576c4c263382bbf6d52c29643d20
Chris McCormick <chris@mccormick.cx>
2015-02-22 20:55:08 +0800

    Applied modifications from latest version of figwheel to allow a single-file js include for dev and minified builds. Found lein-externs and included it to generate the externs file automatically from the cljs source.

--- project.clj
+++ project.clj
@@ -3,42 +3,53 @@
   :url "https://github.com/chr15m/hexatron"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
+  
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2665"]
-                 [figwheel "0.2.2-SNAPSHOT"]
+                 [org.clojure/clojurescript "0.0-2850"]
+                 [figwheel "0.2.5-SNAPSHOT"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
-
+  
   :plugins [[lein-cljsbuild "1.0.4"]
-            [lein-figwheel "0.2.2-SNAPSHOT"]]
-
+            [lein-figwheel "0.2.5-SNAPSHOT"]
+            [lein-externs "0.1.3"]]
+  
   :source-paths ["src"]
   
-  :clean-targets ^{:protect false} ["resources/public/js/compiled/"]
+  :clean-targets ^{:protect false} ["resources/public/js/compiled"]
   
   :cljsbuild {
     :builds [{:id "dev"
-              :source-paths ["src"]
+              :source-paths ["src" "src-dev"]
               :compiler {:output-to "resources/public/js/compiled/hexatron.js"
                          :output-dir "resources/public/js/compiled/out"
                          :optimizations :none
+                         :main hexatron.dev
+                         :asset-path "js/compiled/out"
                          :source-map true
                          :source-map-timestamp true
+                         ;:preamble ["/home/chrism/dev/hexatron/resources/public/lib/rotjs/rot.min.js"] 
+                         ;:externs ["externs/rot.js"]
                          :cache-analysis true }}
              {:id "min"
               :source-paths ["src"]
-              :compiler {:output-to "static-build/hexatron.min.js"
+              :compiler {:output-to "resources/public/js/compiled/hexatron.js"
+                         :main hexatron.core
                          :optimizations :advanced
+                         :externs ["externs/all.js"]
                          :pretty-print false}}]}
-
+  
   :figwheel {
              :http-server-root "public" ;; default and assumes "resources" 
              :server-port 3449 ;; default
              :css-dirs ["resources/public/css"] ;; watch and update CSS
 
+             ;; Start an nREPL server into the running figwheel process
+             ;; :nrepl-port 7888
+
              ;; Server Ring Handler (optional)
              ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server
+             ;; server, this is simple ring servers, if this
+             ;; doesn't work for you just run your own server :)
              ;; :ring-handler hello_world.server/handler
 
              ;; To be able to open files in your editor from the heads up display
---
+++ src-dev/hexatron/dev.cljs
@@ -0,0 +1,11 @@
+(ns hexatron.dev
+    (:require
+     [hexatron.core]
+     [figwheel.client :as fw]))
+
+(fw/start {
+  :websocket-url "ws://localhost:3449/figwheel-ws"
+  :on-jsload (fn []
+               ;; (stop-and-start-my app)
+               )})
+
