bd99a2bfa560eac415503610ea5eb802f0bbef41
Chris McCormick <chris@mccormick.cx>
2015-02-24 18:17:11 +0800

    Got leiningen main entrypoint working.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject postmaker "0.1.0-SNAPSHOT"
+  :description "Hexatron GitHub pages generator"
+  :url "http://chr15m.github.io/hexatron"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main posts
+  )
